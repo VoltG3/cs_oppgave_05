c5e6a6ad1c2f11569bcbcd70a31ad7883f9cf638
VoltG3 <kristiandanoss@gmail.com>
2025-08-22 12:18:02 +0200

    [ UPDATE ] - compose | nix_container

--- flake.nix
+++ flake.nix
@@ -11,20 +11,22 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          config = { allowUnfree = true; };
+          config.allowUnfree = true;
         };
+        lib = pkgs.lib;
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            dotnet-sdk_8
-            aspnetcore-runtime_8
-            mysql80
-            curl
-            bashInteractive
-          ];
+          packages =
+            with pkgs; [
+              dotnet-sdk_8
+              curl
+              bashInteractive
+              mariadb-client
+            ];
 
           DOTNET_ROOT = "${pkgs.dotnet-sdk_8}";
           DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+          DOTNET_MULTILEVEL_LOOKUP = "0";
         };
       });
 }
