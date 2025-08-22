0c747ddad363aa1d7ade03b5c94ce9214208938e
VoltG3 <kristiandanoss@gmail.com>
2025-08-22 12:03:01 +0200

    [ UPDATE ] - compose | nix_container

--- flake.nix
+++ flake.nix
@@ -1,30 +1,30 @@
 {
-  description = "Nix dev shell for movies project (db + api + compose)";
+  description = "cs_oppgave_05 dev shell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            dotnet-sdk
-            aspnetcore-runtime-8
-            mysql80            
-            git
+            dotnet-sdk_8
+            aspnetcore-runtime_8
+            mysql80
             curl
-            jq
-            docker                  
-            make
+            bashInteractive
           ];
-          shellHook = ''
-            echo "Nix dev shell ready. Try: dotnet --info; docker compose ps; mysql --version; jq -V"
-          '';
+
+          DOTNET_ROOT = "${pkgs.dotnet-sdk_8}";
+          DOTNET_CLI_TELEMETRY_OPTOUT = "1";
         };
       });
 }
