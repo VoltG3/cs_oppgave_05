3b713485bfb3d224fa0139cd61de073d600e7e53
VoltG3 <kristiandanoss@gmail.com>
2025-08-25 11:26:02 +0200

    [ UPDATE ] - compose | nix_container | implement #dtest

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "cs_oppgave_05 – minimal devShell only";
+  description = "cs_oppgave_05 – minimal devShell + dtest app";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
@@ -9,21 +9,27 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        dotnet = pkgs.dotnet-sdk_8;
+
+        # dtest clean wrapper without: unpackPhase/src
+        dtestDrv = pkgs.runCommand "dtest" { buildInputs = [ pkgs.makeWrapper dotnet ]; } ''
+          mkdir -p "$out/bin"
+          makeWrapper ${dotnet}/bin/dotnet "$out/bin/dtest" --add-flags "test --nologo"
+        '';
       in
       {
-        devShells = {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              dotnet-sdk_8
-              mariadb-client
-            ];
-          };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ dotnet mariadb-client curl bashInteractive ];
+        };
+
+        apps = {
+          default = { type = "app"; program = "${dtestDrv}/bin/dtest"; };
+          dtest   = { type = "app"; program = "${dtestDrv}/bin/dtest"; };
         };
       }
     );
 }
+
+
 
